dd0957dc1a0a28f377fca62b646932572d2e5831
Divya Raj <draj56227@gmail.com>
2024-09-13 01:09:22 +0530

    mun pages added

--- src/App.js
+++ src/App.js
@@ -42,6 +42,9 @@ import Id_Card from "./components/Profile/PaymentBox/Id_Card";
 
 import MUN from "./components/MUN/Home/Home.js";
 import Home2Reg from "./components/MUNmobile/Registration/Registration";
+import MUNMoreInfoPage1 from "./components/MUN/MoreInfo/Moreinfopage1/Moreinfopage1";
+import MUNMoreInfoPage2 from "./components/MUN/MoreInfo/Moreinfopage2/Moreinfopage2";
+import MUNMoreInfoPage3 from "./components/MUN/MoreInfo/Moreinfopage3/Moreinfopage3";
 import Pevents from "./components/Pevents/Pevents";
 
 // import VideoPlayer from "./components/Home/SECTION3/demo.js";
@@ -146,6 +149,9 @@ useEffect(() => {
           />
           <Route exact={true} path="/mun" element={<MUN/>}/>
           <Route exact={true} path="/MUNregister" element={<Home2Reg/>} />
+          <Route exact={true} path="/muninfo_page1" element={<MUNMoreInfoPage1/>} />
+          <Route exact={true} path="/muninfo_page2" element={<MUNMoreInfoPage2/>} />
+          <Route exact={true} path="/muninfo_page3" element={<MUNMoreInfoPage3/>} />
 
       </Routes>
     </Router>
