cb80fbb6f05214ace225538b570a1f0ca264342f
Dewansh Upadhyay <89184066+dew-op@users.noreply.github.com>
2024-09-18 03:46:46 +0530

    Landing update

--- src/components/Home/SECTION3/Section5.js
+++ src/components/Home/SECTION3/Section5.js
@@ -1,103 +1,92 @@
-import React from 'react'
-import YouTube from 'react-youtube';
-import './sec5.css'
-import bg5 from '../../../assets/bg5 (1).webp'
-import zIndex from '@mui/material/styles/zIndex';
+import React from "react"
+import YouTube from "react-youtube"
+import "./sec5.css"
+import bg5 from "../../../assets/bg5 (1).webp"
+import zIndex from "@mui/material/styles/zIndex"
 // import bg4 from '../../../assets/bgs4.svg'
 
 const Section5 = () => {
-    const videoOptions = {
-        height: '900',
-        width: '100',
-        playerVars: {
-          // autoplay: 1,
-        },
-      };
+	const videoOptions = {
+		height: "900",
+		width: "100",
+		playerVars: {
+			// autoplay: 1,
+		},
+	}
 
-      const opts = {
-        height: '390',
-        width: '640',
-        // playerVars: {
-        //   // https://developers.google.com/youtube/player_parameters
-        //   autoplay: 1,
-        // },
-      };
-    
-      return (
-        <div className="background-container">
+	const opts = {
+		height: "390",
+		width: "640",
+		// playerVars: {
+		//   // https://developers.google.com/youtube/player_parameters
+		//   autoplay: 1,
+		// },
+	}
 
-<img src={bg5} alt=""  className='bg-s5' style={{position:'absolute', zIndex:'0', width:'100%'}}/>
-          <div className="video-mask">
-          {/* <YouTube videoId="rVsg7yOYN44" opts={videoOptions} className="youtube-video" /> */}
+	return (
+		<div className='background-container'>
+			<img
+				src={bg5}
+				alt=''
+				className='bg-s5'
+				style={{ position: "absolute", zIndex: "0", width: "100%" }}
+			/>
+			<div className='video-mask'>
+				{/* <YouTube videoId="rVsg7yOYN44" opts={videoOptions} className="youtube-video" /> */}
 
+				{/* <img src={bg4} alt="" id='image1' /> */}
+				<p style={{ zIndex: "1000" }}>Thomso'23 Aftermovie</p>
+			</div>
 
-          
+			<div style={styles.container}>
+				<div style={styles.videoWrapper}>
+					<iframe
+						width='560'
+						height='315'
+						src='https://www.youtube.com/embed/rVsg7yOYN44?rel=0&controls=0&modestbranding=1' // Replace with your video URL
+						title='YouTube video player'
+						frameBorder='0'
+						allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+						// allowFullScreen
+						// controls='0'
+						// rel='0'
+						style={styles.video}
+					></iframe>
+				</div>
+			</div>
+		</div>
+	)
 
+	// return(
+	//     <div>
+	//         <img src={bg4} alt="" />
 
-
-
-            
-            
-
-            {/* <img src={bg4} alt="" id='image1' /> */}
-            <p style={{zIndex:'1000'}}>Thomso'23 Aftermovie</p>
-          </div>
-
-          <div style={styles.container}>
-            
-      <div style={styles.videoWrapper}>
-        <iframe
-          width="560"
-          height="315"
-          src="https://www.youtube.com/embed/rVsg7yOYN44?rel=0&controls=0&modestbranding=1" // Replace with your video URL
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          // allowFullScreen
-          // controls='0'
-          // rel='0'
-          style={styles.video}
-        ></iframe>
-      </div>
-    </div>
-        </div>
-      );
-
-
-    // return(
-    //     <div>
-    //         <img src={bg4} alt="" />
-
-
-            
-    //     </div>
-    // )
+	//     </div>
+	// )
 }
 
-
 const styles = {
-  container: {
-    // position:'fixed',
-    paddingTop:'35vw',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '40.64vw',
-    // backgroundColor: '#f9f9f9',
-    backgroundColor:'transparent',
-    // zIndex:'1000'
-  },
-  videoWrapper: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    zIndex:'1000',
-    marginLeft:'4vw'
-  },
-  video: {
-    width:'44vw',  // Width smaller than height
-    height: '32vw', // Height larger than width
-  },
-};
+	container: {
+		paddingTop: "35vw",
+		display: "flex",
+		justifyContent: "center",
+		alignItems: "center",
+		height: "40.64vw",
+		// backgroundColor: '#f9f9f9',
+		backgroundColor: "transparent",
+		// zIndex:'1000'
+	},
+	videoWrapper: {
+		display: "flex",
+		justifyContent: "center",
+		alignItems: "center",
+		zIndex: "1000",
+		marginLeft: "4vw",
+	},
+	video: {
+		width: "44vw", // Width smaller than height
+		height: "32vw", // Height larger than width
+	},
+}
 
 export default Section5
--- src/components/Home/SECTION3/section3.js
+++ src/components/Home/SECTION3/section3.js
@@ -1,63 +1,141 @@
-import React from 'react'
-import './section3.css'
-import bg from './../../../assets/bg_peacock.webp'
-import title from './../../../assets/s3_title.webp'
-import celeb from './../../../assets/Celebs.webp'
+import React from "react"
+import "./section3.css"
+import bg from "./../../../assets/bg_peacock.webp"
+import title from "./../../../assets/s3_title.webp"
+import celeb from "./../../../assets/Celebs.webp"
 // import clouds from './../../../assets/clouds.svg'
-import Section4 from './Section4'
+import Section4 from "./Section4"
 // import title4 from './../../../assets/title_s4.svg'
-import Section5 from './Section5'
-import pattern from './../../../assets/pattern.webp'
+import Section5 from "./Section5"
+import pattern from "./../../../assets/pattern.webp"
 // import Footer from '../../Footer/Footer'
-
-
+import mobsecs from "../../../assets/mobsection3.webp"
+import YouTube from "react-youtube"
 
 const Section2 = () => {
+	const videoOptions = {
+		height: "900",
+		width: "100",
+		playerVars: {
+			// autoplay: 1,
+		},
+	}
 
+	const opts = {
+		height: "390",
+		width: "640",
+	}
 
-  return (
-    <div className='' style={{width:'100vw', zIndex:'101'}}>
-        <img style={{height:'150vw', width:'100vw', zIndex:'-10', position:'absolute', objectFit:'contain'}}  alt ="bgbg"src={bg}/>
+  const styles = {
+    container: {
+		  position: "absolute",
+      bottom: "480px",
+      left: "80px",
+      paddingTop: "35vw",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "40.64vw",
+      // backgroundColor: '#f9f9f9',
+      backgroundColor: "transparent",
+      // zIndex:'1000'
+    },
+    videoWrapper: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      zIndex: "1000",
+    },
+    video: {
+      width: "250px", // Width smaller than height
+      height: "170px", // Height larger than width
+    },
+  }
 
-        {/* <div className='heading_s3' style={{color:'red', zIndex:'100', fontSize:'10vw', position:'fixed'}}>
+	return (
+		<div className='' style={{ width: "100vw", zIndex: "101" }}>
+			<img
+				style={{
+					height: "150vw",
+					width: "100vw",
+					zIndex: "-10",
+					position: "absolute",
+					objectFit: "contain",
+				}}
+				alt='bgbg'
+				src={bg}
+			/>
+
+			{/* <div className='heading_s3' style={{color:'red', zIndex:'100', fontSize:'10vw', position:'fixed'}}>
             
         </div> */}
-        <div className='wrapper_s3' style={{display:'flex', flexDirection:'column', alignItems:'center', paddingTop:'16vw'}} alt="bgbgb">
-          <div>
-          <img src={title} style={{ width:'80vw', }}/>
-          </div>
-          <div className='celeb_pic' >
-            <img src={celeb} alt=""  style={{width:'60vw'}}/>
-          </div>
-        </div>
-        
+			<div
+				className='wrapper_s3'
+				style={{
+					display: "flex",
+					flexDirection: "column",
+					alignItems: "center",
+					paddingTop: "16vw",
+				}}
+				alt='bgbgb'
+			>
+				<div>
+					<img src={title} style={{ width: "80vw" }} />
+				</div>
+				<div className='celeb_pic'>
+					<img src={celeb} alt='' style={{ width: "60vw" }} />
+				</div>
+			</div>
 
-        <div className='wrapper_s4' >
-          <div style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
-            {/* <img src={title4} alt="" style={{position:'absolute', marginTop:'85vw', zIndex:'100',paddingTop:'5vw'}}/>
-             */}
+			<div className='mobwrapper_s4'>
+				<div style={styles.container}>
+					<div style={styles.videoWrapper}>
+						<iframe
+							width='560'
+							height='315'
+							src='https://www.youtube.com/embed/rVsg7yOYN44?rel=0&controls=0&modestbranding=1' // Replace with your video URL
+							title='YouTube video player'
+							frameBorder='0'
+							allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+							// allowFullScreen
+							// controls='0'
+							// rel='0'
+							style={styles.video}
+						></iframe>
+					</div>
+				</div>
+			</div>
 
-             <p className='title_s4'>New Events</p>
-          </div>
-          <div style={{zIndex:'1000', marginTop:'39vw', width:'99.4vw'}}>
-          <Section4/>
-          </div>
+			<div className='wrapper_s4'>
+				<div
+					style={{
+						display: "flex",
+						justifyContent: "center",
+						alignItems: "center",
+					}}
+				>
+					<p className='title_s4'>New Events</p>
+				</div>
+				<div style={{ zIndex: "1000", marginTop: "39vw", width: "99.4vw" }}>
+					<Section4 />
+				</div>
 
-          <div style={{position:'absolute', marginTop:'59vw',width:'100%', zIndex:'1000'}}>
-            <img src={pattern} alt="" style={{width:'98%'}}/>
-          </div>
-          <div className='wrapper_s5' style={{marginTop:'105vw'}}>
-          <Section5/>
-          </div>
-          
-          {/* <Footer/> */}
-        </div>
-
-       
-
-        
-    </div>
-  )
+				<div
+					style={{
+						position: "absolute",
+						marginTop: "59vw",
+						width: "100%",
+						zIndex: "1000",
+					}}
+				>
+					<img src={pattern} alt='' style={{ width: "98%" }} />
+				</div>
+				<div className='wrapper_s5' style={{ marginTop: "105vw" }}>
+					<Section5 />
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Section2
