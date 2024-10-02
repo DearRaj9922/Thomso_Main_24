2daab386fea58686a9ab4f92f2dc47604704c62a
Dhruv Pankaj <dhruv.dpj@gmail.com>
2024-10-02 17:32:21 +0530

    id_Card

--- src/components/PdfRenderer/PdfDownload.js
+++ src/components/PdfRenderer/PdfDownload.js
@@ -1,47 +1,59 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { PDFDownloadLink, PDFViewer } from "@react-pdf/renderer";
 import Renderer from "./Renderer";
 import { connect } from "react-redux";
-import { fetchUser, logout } from "../User/UserActions";
-import { Store } from "../../Config/Store";
-import loader from "../../components/Newloader/Newloader.js"
-
+import { fetchUser } from "../User/UserActions";
 import "./Pdfcss.css";
 
-const PdfDownload = ({ userDetails, loading }) => { // Destructure props correctly here
+const PdfDownload = ({ userDetails, loading }) => {
+  const [isMobile, setIsMobile] = useState(false);
 
+  // Detect if the user is on mobile
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768); // Define mobile width as 768px or less
+    };
 
-  // const func = () => {
-  //   const { dispatch } = Store;
-  //   dispatch(fetchUser());
-  // }
-  useEffect(()=>{
-    // console.log("lauda",userDetails)
-  },[])
+    // Check on component mount
+    handleResize();
+
+    // Add event listener to handle window resize
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup event listener on unmount
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   return (
-    <>
-      <div className="laptop">
-        
-        <PDFViewer style={{ width: "100vw", height: "100vh" }}>
-          <Renderer user={userDetails} /> 
-        </PDFViewer>
-      </div>
-      <div className="mobile">
-                             
-        <div>
-          <PDFDownloadLink document={<Renderer user={userDetails} />} fileName='Id Card'>
-            {({ loading }) =>
-              loading ? (
-                <button>Loading document...</button>
-              ) : (
-                <button>Download</button>
-              )   
-            }
-          </PDFDownloadLink>
+      <>
+        {/* Laptop/Desktop View */}
+        <div className="laptop">
+          <PDFViewer style={{ width: "100vw", height: "100vh" }}>
+            <Renderer user={userDetails} />
+          </PDFViewer>
         </div>
-      </div>
-    </>
+
+        {/* Mobile View */}
+        {isMobile && (
+            <div className="mobile">
+              <div>
+                {/* Provide a download link on mobile */}
+                <PDFDownloadLink
+                    document={<Renderer user={userDetails} />}
+                    fileName='IdCard.pdf'
+                >
+                  {({ loading }) =>
+                      loading ? (
+                          <button>Loading document...</button>
+                      ) : (
+                          <button>Download PDF</button>
+                      )
+                  }
+                </PDFDownloadLink>
+              </div>
+            </div>
+        )}
+      </>
   );
 }
 
