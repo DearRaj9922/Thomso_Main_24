a1db8935c51b1c21edafb66c7cf756a542c40a1e
Nakshatra Goel <139595090+Nakshatra05@users.noreply.github.com>
2024-10-24 19:40:32 +0530

    Update SponsorsGrid.js

--- src/components/Sponsers/SponsorsGrid.js
+++ src/components/Sponsers/SponsorsGrid.js
@@ -1,50 +1,68 @@
 import React from 'react';
 import './SponsorsGrid.css';
 import sampleLogo from './images/sample.png'; // Use actual logos for each sponsor
+import visionias from './images/visionias.png'; // Use actual logos for each sponsor
+import sbi from './images/sbi.png'; // Use actual logos for each sponsor
+import powergrid from './images/powergrid.jpg'; // Use actual logos for each sponsor
+import adani from './images/adani.jpg';
+import gail from './images/gail.jpg';
+import rvnl from './images/rvnl.jpg';
+import bonkers from './images/bonkers.png';
+import nxtinfra from './images/nxtinfra.jpg';
+import rollsking from './images/rollsking.jpg';
+import qelica from './images/qelica.jpg';
+import sanjeevani from './images/sanjeevani.jpg';
+import iocl from './images/iocl.jpg';
+import horiba from './images/horiba.jpg';
+import canara from './images/canara.png';
+import thdc from './images/thdc.jpg';
+import krafton from './images/krafton.png';
+import beardo from './images/beardo.png';
+import pnb from './images/pnb.jpg';
 
 const SponsorsGrid = () => {
     return (
         <div className="sponsors-grid-container">
             <h2>OUR SPONSORS</h2>
 
-            <h3>Title Sponsors</h3>
+            {/* <h3>Title Sponsors</h3> */}
             <div className="sponsors-row">
                 <div className="sponsor">
-                    <img src={sampleLogo} alt="UniAcco" />
+                    <img src={visionias} alt="UniAcco" />
                 </div>
                 <div className="vertical-divider"></div>
                 <div className="sponsor">
-                    <img src={sampleLogo} alt="UniCreds" />
+                    <img src={sbi} alt="UniCreds" />
                 </div>
                 <div className="vertical-divider"></div>
                 <div className="sponsor">
-                    <img src={sampleLogo} alt="UniScholarz" />
+                    <img src={powergrid} alt="UniScholarz" />
                 </div>
             </div>
 
-            <h3>Title Sponsors</h3>
+            {/* <h3>Title Sponsors</h3> */}
             <div className="sponsors-row">
                 <div className="sponsor">
-                    <img src={sampleLogo} alt="UniAcco" />
+                    <img src={gail} alt="UniAcco" />
                 </div>
                 <div className="vertical-divider"></div>
                 <div className="sponsor">
-                    <img src={sampleLogo} alt="UniCreds" />
+                    <img src={rvnl} alt="UniCreds" />
                 </div>
                 <div className="vertical-divider"></div>
                 <div className="sponsor">
-                    <img src={sampleLogo} alt="UniScholarz" />
+                    <img src={bonkers} alt="UniScholarz" />
                 </div>
             </div>
 
-            <h3 className='beverage'>Beverage Partners</h3>
+            {/* <h3 className='beverage'>Beverage Partners</h3> */}
             <div className="sponsors-row">
                 <div className="sponsor">
-                    <img src={sampleLogo} alt="UniAcco" />
+                    <img src={nxtinfra} alt="UniAcco" />
                 </div>
                 <div className="vertical-divider"></div>
                 <div className="sponsor">
-                    <img src={sampleLogo} alt="UniScholarz" />
+                    <img src={rollsking} alt="UniScholarz" />
                 </div>
             </div>
         </div>
